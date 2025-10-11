24745f2689a6686c2b1a4fc2c6708bf6b25c3d6a
Olivier <olivier.bouet-willaumez@u-pec.fr>
2025-10-11 18:17:39 +0200

    refonte totale

--- js/email.js
+++ js/email.js
@@ -2,3 +2,5 @@ const user = "olivier.bouet-willaumez";
 const domain = "u-pec.fr";
 const emailElement = document.getElementById("email");
 emailElement.innerHTML = user + "@" + domain;
+const emailAddress = `${user}@${domain}`;
+emailElement.innerHTML = `<a href="mailto:${emailAddress}" class="contact-link">${emailAddress}</a>`;
