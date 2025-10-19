172a711c851991045664d268aa54ccb751904e8c
Olivier <olivier.bouet-willaumez@u-pec.fr>
2025-10-19 18:02:52 +0200

    modif js email

--- js/email.js
+++ js/email.js
@@ -1,6 +1,12 @@
-const user = "olivier.bouet-willaumez";
-const domain = "u-pec.fr";
-const emailElement = document.getElementById("email");
-emailElement.innerHTML = user + "@" + domain;
-const emailAddress = `${user}@${domain}`;
-emailElement.innerHTML = `<a href="mailto:${emailAddress}" class="contact-link">${emailAddress}</a>`;
+window.addEventListener("DOMContentLoaded", () => {
+  const user = "olivier.bouet-willaumez";
+  const domain = "u-pec.fr";
+  const emailElement = document.getElementById("email");
+
+  if (emailElement) {
+    const emailAddress = `${user}@${domain}`;
+    emailElement.innerHTML = `<a href="mailto:${emailAddress}" class="contact-link">${emailAddress}</a>`;
+  } else {
+    console.warn("L’élément #email est introuvable.");
+  }
+});
